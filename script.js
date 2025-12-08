82fab3c0f74e18b0f1a6a37282bc036907a166c4
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2025-12-08 14:45:33 -0700

    feat: Display movies after fetching data based on the search input

--- script.js
+++ script.js
@@ -3,13 +3,34 @@ const searchBtn = document.querySelector(".search-btn")
 
 searchBtn.addEventListener("click", getMovieData)
 
+function displayMovies(movies) {
+    const movieContainer = document.querySelector(".movie-container")
+    movieContainer.innerHTML = ""
+
+    movies.forEach((movie) => {
+
+        const movieDiv = document.createElement("div")
+
+        movieDiv.innerHTML = `
+            <img src="${movie.Poster}" />
+            <div>
+                <h3>${movie.Title}</h3>
+            </div>
+        `
+
+        movieContainer.appendChild(movieDiv)
+    })
+}
+
 function getMovieData() {
     const movieTitle = input.value.trim()
     const url = `http://www.omdbapi.com/?apikey=e08693bc&s=${movieTitle}`
-    console.log(movieTitle)
     console.log("Fetching url", url)
 
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data.Search)
+            displayMovies(data.Search)
+        })
 }
