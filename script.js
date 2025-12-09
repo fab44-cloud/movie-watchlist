ee30d65af7fc364abf51f96fc74848cfdaec9fe6
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2025-12-08 19:27:07 -0700

    Display movie run time

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
 const input = document.querySelector(".movie-input")
 const searchBtn = document.querySelector(".search-btn")
+const movieContainer = document.querySelector(".movie-container")
 
 searchBtn.addEventListener("click", getMovieData)
 
-function displayMovies(movies) {
-    const movieContainer = document.querySelector(".movie-container")
-    movieContainer.innerHTML = ""
-
-    movies.forEach((movie) => {
-
-        const movieDiv = document.createElement("div")
-
-        movieDiv.innerHTML = `
-            <img src="${movie.Poster}" />
-            <div>
-                <h3>${movie.Title}</h3>
-            </div>
-        `
+function displayMovies(movie) {
+    const movieDiv = document.createElement("div")
+    
+    movieDiv.innerHTML = `
+        <img src="${movie.Poster}" />
+        <div>
+            <h3>${movie.Title}</h3>
+            <p>${movie.Runtime}<p>
+        </div>
+    `
 
         movieContainer.appendChild(movieDiv)
-    })
 }
 
 function getMovieData() {
     const movieTitle = input.value.trim()
-    const url = `http://www.omdbapi.com/?apikey=e08693bc&s=${movieTitle}`
-    console.log("Fetching url", url)
+    const searchUrl = `http://www.omdbapi.com/?apikey=e08693bc&s=${movieTitle}`
+    console.log("Fetching url", searchUrl)
+    movieContainer.innerHTML = ""
 
-    fetch(url)
+    fetch(searchUrl)
         .then(res => res.json())
         .then(data => {
-            console.log(data.Search)
-            displayMovies(data.Search)
+            if (data.Response === "True" && data.Search) {
+                data.Search.forEach(movieSummary => {
+                    console.log(movieSummary.imdbID)
+                    fetchMovieDetails(movieSummary.imdbID)
+                })
+            } else {
+                movieContainer.textContent = "Unable to find what you're looking for. Please try another search."
+            }
+            console.log(data)
+            // displayMovies(data.Search)
         })
+        .catch(error => console.error("Search fetch error", error))
 }
 
 function fetchMovieDetails(imdbID) {
@@ -42,10 +48,9 @@ function fetchMovieDetails(imdbID) {
         .then(res => res.json())
         .then(movieDetails => {
             console.log(movieDetails)
-            if (movieDetails.response === "True") {
+            if (movieDetails.Response === "True") {
+                console.log(movieDetails)
                 displayMovies(movieDetails)
             }
         })
 }
-
-fetchMovieDetails("tt0083658")
