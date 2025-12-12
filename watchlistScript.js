e8a171efbf46f451281eb0151a10552058890416
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2025-12-11 19:17:45 -0700

    feat: Display movie data from local storage Check if the length of the 'watchMovies' array is greater than 0

--- watchlistScript.js
+++ watchlistScript.js
@@ -3,12 +3,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (storedWatchlistString) {
         const watchlistMovies = JSON.parse(storedWatchlistString)
-        console.log(typeof(watchlistMovies))
 
-        if (watchlistMovies > 0) {
+        if (watchlistMovies.length > 0) {
             watchlistMovies.forEach(movieData => {
                 displayWatchlistItem(movieData)
             })
-        }
+        } 
     }
 })
+
+function displayWatchlistItem(movieData) {
+    const movieContainer = document.querySelector(".movie-container")
+    const movieDiv = document.createElement("div")
+    movieDiv.classList.add("movie-card")
+    
+    movieDiv.innerHTML = `
+        <img src="${movieData.Poster}" alt="${movieData.Title}" />
+        <div>
+            <div class="card-row">
+                <h3 class="movie-title">${movieData.Title}</h3>
+                <i class="fa-solid fa-star"></i>
+                <span class="movie-rating">${movieData.imdbRating}</span>
+            </div>
+            <div class="card-row">
+                <p class="movie-runtime">${movieData.Runtime}</p>
+                <p class="movie-genre">${movieData.Genre}</p>
+                <div class="movie-watchlist">
+                    <i class="fa-solid fa-circle-plus"></i>
+                    <span>Remove</span>
+                </div>
+            </div>
+            <p class="movie-plot">${movieData.Plot}</p>
+        </div>
+    `
+
+    movieContainer.appendChild(movieDiv)
+}
